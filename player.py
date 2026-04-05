9f37cbf4da5161ac35cf997a96df2d33a692686b
Makkiiew <chisholmmmm@gmail.com>
2026-04-05 19:30:05 +0900

    K: add timer cooldown

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 from shot import Shot
 import pygame
 
@@ -8,7 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-
+        self.timer = 0
     
     # in the Player class
     def triangle(self):
@@ -31,6 +31,7 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
     
     def update(self, dt):
+        self.timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -51,6 +52,9 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
     
     def shoot(self):
+        if self.timer > 0:
+            return
+        self.timer = PLAYER_SHOOT_COOLDOWN_SECONDS
         new_shot = Shot(self.position.x, self.position.y)
         velocity_vector = pygame.Vector2(0, 1).rotate(self.rotation)
         new_shot.velocity = velocity_vector * PLAYER_SHOT_SPEED
